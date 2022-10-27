7ac4eb935cedc17a4523e808a60fc0fdd8b0c2b6
acfarrell <audfarrell@ucla.edu>
2022-10-27 13:06:29 -0700

    adding input deck and batch scripts for running on cluster with sbatch

---
+++ input/ae98_2022.py
@@ -0,0 +1,52 @@
+'''
+Define all the input parameters for the analysis in this file!
+'''
+# Define the path to the data you want to analyze
+data_directory = '/global/homes/o/oods/inverse_shadowgraphy/2022_data/160psi-data-new/'
+
+# Define the source plane image file. Passing None will assume a uniform background
+source_image_file = None
+
+# Rather than initializing new sites on the source plane, you can use an existing
+# power diagram as the source by feeding in the saved data from a previous run
+use_previous_source = False
+# If True, give the path to the .npz file to load:
+previous_source = 'output/output_test.npz'
+
+# Define the target plane image file you want to analyze.
+target_image_file = 'co2_27.4mJ_delayRail205mm_micro8.5mm_PMQs41mm_h2_164.2psig_source_image.png'
+
+# Define the number of sites you'd like to use
+# If passed None or omitted the default is 0.8 * N_pixels in the image
+N_sites = None
+
+# Define the threshold for Lloyd relaxation on the source plane
+# If passed None or omitted the default is .1 (10%)
+lloyd_threshold = 0.1
+
+# By default all the output will be saved as a single .npz numpy file (to read into other python scripts)
+# To also save the raw data in several .txt files, set save_txt to True
+save_txt = False
+
+# Define the directory where you want the results of the analysis saved
+output_directory = '/global/homes/o/oods/inverse_shadowgraphy/2022_data/'
+
+# Define the suffix you'd like to put at the end of any output filenames
+# For example output_suffix = 'test' would change 'log.txt' to 'log_test.txt'
+# Passing None will default to using the current date and time when the script is run
+output_suffix = 'inverted'
+
+# Give the filename where you'd like the script to log its system output.
+# If passed None the code will print to the console as it runs.
+log_file = 'log.txt'
+
+# Set the desired logging level for the run. The possible levels are:
+# DEBUG: Detailed information needed when diagnosing problems
+# INFO: Confirmation that thigs are working as expectsd
+# WARNING: Indications that something unexpected has happened but the code is still running
+# ERROR: Something bad happened and a function was not able to be completed
+# CRITICAL: Something really bad happened and the whole program cannot finish running
+
+# The default logging level is INFO
+log_level = 'INFO'
+
---
+++ submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=6:00:00
+#SBATCH --constraint=knl
+#SBATCH --tasks-per-node=68
+#SBATCH --qos=regular
+#SBATCH --job-name=ae98_inverse_shadowgraphy
+#SBATCH --error=%x-%j.err
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=oods@nersc.gov
+#SBATCH --mail-type=ALL
+
+# set up for problem & define any environment variables here
+module load python
+conda activate shadow_2021_jup
+
+srun -n 1 --cpu-bind=none python invert.py ae98_2022
+
+# perform any cleanup or short post-processing here
